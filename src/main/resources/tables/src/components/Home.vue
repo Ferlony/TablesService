ee38dd817c1d97e4119ddf0a7453aefdbdd0d992
Ferlony <Hegecioum@gmail.com>
2023-05-23 00:46:36 +0500

    bp before mk sheets

--- src/main/resources/tables/src/components/Home.vue
+++ src/main/resources/tables/src/components/Home.vue
@@ -1,10 +1,13 @@
 <template>
-  <div class="container">
-    <header class>
-      <h3>{{ content }}</h3>
-    </header>
+  <div class="col-md-8 offset-md-2">
+    <div class="container">
+      <header class="text-align center-screen">
+        <h3>{{ content }}</h3>
+      </header>
+    </div>
   </div>
 </template>
+
 
 <script>
 import UserService from "../services/user.service";
@@ -17,20 +20,42 @@ export default {
     };
   },
   mounted() {
-    UserService.getPublicContent().then(
-      (response) => {
-        this.content = response.data;
-      },
-      (error) => {
-        this.content =
-          (error.response &&
-            error.response.data &&
-            error.response.data.message) ||
-          error.message ||
-          error.toString();
-      }
-    );
+    try {
+      UserService.getPublicContent().then(
+        (response) => {
+          this.content = response.data;
+        },
+        (error) => {
+          this.content =
+            (error.response &&
+              error.response.data &&
+              error.response.data.message) ||
+            error.message ||
+            error.toString();
+
+        }
+      );
+    }
+    catch (e) {
+      console.log(e);
+      this.content = "Authtorize to view conntent";
+    }
   },
 };
 </script>
 
+
+<style scoped>
+.text-align{
+  text-align: center;
+}
+
+.center-screen {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+  min-height: 50vh;
+}
+</style>
